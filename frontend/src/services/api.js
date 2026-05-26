6ce279e764962873005e7cca32b3a59eb83b9722
tomasabcantuaria@gmail.com <tomasabcantuaria@gmail.com>
2026-05-26 17:38:19 -0300

    fix: adiciona export getCryptoPrice no api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,6 +12,8 @@ export const getCurrencyBySymbol = (symbol) => api.get(`/currencies/${symbol}`)
 
 export const getCryptos = () => api.get('/cryptos')
 
+export const getCryptoPrice = (id) => api.get(`/cryptos/${id}`)
+
 export const getSummary = () => api.get('/summary')
 
 export const getAvailableCurrencies = () => api.get('/currencies/available')
